71e8905b10d4762f82098ab6d85d641f40403902
Okano Takashi <kjm-okano-t@kajima-cloud.jp>
2022-03-07 07:34:51 +0000

    prettier修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,7 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-  ],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'plugin:prettier/recommended', 'prettier'],
   plugins: ['vue', 'prettier'],
   // add your custom rules here
   rules: {},
