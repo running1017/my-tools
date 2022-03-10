ac9fc6591d77bf2d47e8399420926f68aecf6c02
Okano Takashi <kjm-okano-t@kajima-cloud.jp>
2022-03-10 23:35:55 +0000

    スライダー目盛り修正

--- components/TimeSelector.vue
+++ components/TimeSelector.vue
@@ -2,64 +2,62 @@
   <v-container fluid class="pb-1 mb-2">
     <v-row align="end">
       <v-col>
-        <v-container fluid class="ma-0 pa-0">
-          <v-row>
-            <v-col align-self="start" cols="12" md="3" lg="4" xl="3">
-              <v-subheader>日付選択{{ breakpoint }}</v-subheader>
-              <v-menu
-                ref="menu"
-                v-model="menu"
-                :close-on-content-click="false"
-                transition="slide-y-transition"
-                min-width="auto"
-              >
-                <template #activator="{ attrs, on }">
-                  <v-text-field
-                    v-model="formattedDate"
-                    prepend-icon="mdi-calendar"
-                    v-bind="attrs"
-                    dense
-                    color="teal darken-2"
-                    readonly
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="date"
+        <v-row>
+          <v-col align-self="start" cols="12" md="3" lg="4" xl="3">
+            <v-subheader v-if="!$vuetify.breakpoint.xs">日付選択</v-subheader>
+            <v-menu
+              ref="menu"
+              v-model="menu"
+              :close-on-content-click="false"
+              transition="slide-y-transition"
+              min-width="auto"
+            >
+              <template #activator="{ attrs, on }">
+                <v-text-field
+                  v-model="formattedDate"
+                  prepend-icon="mdi-calendar"
+                  v-bind="attrs"
+                  dense
                   color="teal darken-2"
-                  locale="ja"
-                  show-adjacent-months
-                  :day-format="(date) => new Date(date).getDate()"
-                  @change="save"
-                ></v-date-picker>
-              </v-menu>
-            </v-col>
-            <v-col align-self="start">
-              <v-subheader>時刻選択</v-subheader>
-              <v-range-slider
-                v-model="timeRange"
-                :min="startTime"
-                :max="endTime"
-                :step="stepMinutes / 60"
-                color="teal darken-1"
-                thumb-label="always"
-                thumb-size="40"
-                ticks
-                tick-size="4"
-                :tick-labels="timeLabels"
-              >
-                <template #thumb-label="{ value: sliderValue }">
-                  {{ timeFormatter(sliderValue) }}
-                </template>
-              </v-range-slider>
-            </v-col>
-          </v-row>
-        </v-container>
+                  readonly
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="date"
+                color="teal darken-2"
+                locale="ja"
+                show-adjacent-months
+                :day-format="(date) => new Date(date).getDate()"
+                @change="save"
+              ></v-date-picker>
+            </v-menu>
+          </v-col>
+          <v-col align-self="start">
+            <v-subheader v-if="!$vuetify.breakpoint.xs">時刻選択</v-subheader>
+            <v-range-slider
+              v-model="timeRange"
+              :min="startTime"
+              :max="endTime"
+              :step="stepMinutes / 60"
+              color="teal darken-1"
+              thumb-label="always"
+              :thumb-size="$vuetify.breakpoint.xs ? 30 : 40"
+              ticks
+              tick-size="4"
+              :tick-labels="timeLabels"
+            >
+              <template #thumb-label="{ value: sliderValue }">
+                {{ timeFormatter(sliderValue) }}
+              </template>
+            </v-range-slider>
+          </v-col>
+        </v-row>
       </v-col>
 
       <v-divider vertical class="my-2"></v-divider>
 
-      <v-col cols="1" align-self="center">
+      <v-col cols="2" sm="1" align-self="center">
         <v-btn icon @click="deleteValue">
           <v-icon>mdi-close</v-icon>
         </v-btn>
@@ -99,9 +97,6 @@ export default {
     menu: false,
   }),
   computed: {
-    breakpoint() {
-      return this.$vuetify.breakpoint.name
-    },
     date: {
       get() {
         return this.value.date
@@ -111,10 +106,13 @@ export default {
       },
     },
     timeLabels() {
-      const length = ((this.endTime - this.startTime) / this.stepMinutes) * 60
+      const length = ((this.endTime - this.startTime) / this.stepMinutes) * 60 + 1
       const labels = Array(length)
-      labels[0] = timeFormatter(this.startTime)
-      labels.push(timeFormatter(this.endTime))
+      for (let t = this.startTime; t <= this.endTime; t++) {
+        if (this.endTime - this.startTime <= 10 || t % 2 === 0) {
+          labels[((t - this.startTime) * 60) / this.stepMinutes] = String(t)
+        }
+      }
       return labels
     },
     timeRange: {
