f22469994b02ce38553c77367ca70d867abfe944
running1017 <running1017@gmail.com>
2022-03-24 07:37:56 +0000

    test名称

--- src/jest/schedule.test.ts
+++ src/jest/schedule.test.ts
@@ -5,7 +5,7 @@ describe.each([
   ['2022-03-09', true, '03/09（水）'],
   ['2022-03-09', false, '3/9（水）'],
 ])('dateFormatter', (date, padding, expected) => {
-  test(`padding ${padding ? 'あり' : 'なし'}`, () => {
+  test(`padding${padding ? 'あり' : 'なし'}`, () => {
     expect(dateFormatter(date, padding)).toBe(expected)
   })
 })
@@ -19,7 +19,7 @@ describe.each([
   [12.25, true, '12:15'],
   [12.25, false, '12:15'],
 ])('timeFormatter', (time, padding, expected) => {
-  test(`padding ${padding ? 'あり' : 'なし'}`, () => {
+  test(`return ${expected} padding${padding ? 'あり' : 'なし'}`, () => {
     expect(timeFormatter(time, padding)).toBe(expected)
   })
 })
@@ -37,8 +37,8 @@ describe.each([
     '、',
     '01:30～03:00、10:00～15:15',
   ],
-])('timeListFormatter', (time, padding, separator, comma, expected) => {
-  test(`padding ${padding ? 'あり' : 'なし'}`, () => {
-    expect(timeListFormatter(time, padding, separator, comma)).toBe(expected)
+])('timeListFormatter', (timeList, padding, separator, comma, expected) => {
+  test(`return ${expected}`, () => {
+    expect(timeListFormatter(timeList, padding, separator, comma)).toBe(expected)
   })
 })
